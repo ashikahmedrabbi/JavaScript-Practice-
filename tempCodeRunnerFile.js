071c575c54abec601b9470ad447bbf32ffbe8f9d
unknown <ashikahmedrabbi@gmail.com>
2023-11-08 23:55:04 +0600

    Day Two

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,5 +1,6 @@
-let fact = 1;
-for (let i = 1; i <= 70; i++) {
-    fact *= i;
-
-} console.log(fact);
+let arr=[1,2,3,4,5,6,7,8,9,10];
+let sum =0;
+for (let i =0; i<arr.length;i++){
+    sum+=arr[i];
+    
+}console.log(sum);
--- try.js
+++ try.js
@@ -23,10 +23,41 @@
 //     console.log(txt);
 // }
 
-const cars = [10, 20, 30];
+// const cars = [10, 20, 30];
 
-let text = "";
-for (let x of cars) {
-    text += x;
+// let text = "";
+// for (let x of cars) {
+//     text += x;
 
-} console.log(text);
+// } console.log(text);
+
+
+// Coding challenge #8: Create a function that will convert from Celsius to Fahrenheit
+
+// function celsiusToFar(data) {
+
+//     let fahrenheit = data * 9 / 5 + 32;
+//     return fahrenheit;
+// }
+
+// let fahrenheit = celsiusToFar(10);
+// console.log(fahrenheit); 
+
+// Coding challenge #9: Create a function that will convert from Fahrenheit to Celsius
+
+// function fahrenheit(data) {
+//     let celsius = (data - 32) * 5 / 9;
+//     return celsius;
+// }
+
+// console.log(fahrenheit(10));
+
+// Coding challenge #10: Calculate the sum of numbers in an array of numbers
+
+// let arr=[1,2,3,4,5,6,7,8,9,10];
+// let sum =0;
+// for (let i =0; i<arr.length;i++){
+//     sum+=arr[i];
+    
+// }console.log(sum);
+
